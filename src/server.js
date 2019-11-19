b7b829e9d8b54e63200ab2619f1e812744728813
Todd Jones <atoddj@gmail.com>
2019-11-19 12:00:21 -0500

    Added models to request, connected db

--- src/server.js
+++ src/server.js
@@ -1,14 +1,29 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import cors from 'cors';
+import models, {connectDb} from './models';
+import routes from './routes';
+import 'dotenv/config';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.use(bodyParser.json());
+app.use(async (req, res, next) => {
+    req.context = {
+      models
+    };
+    next();
+  });
 
 app.get('/',(req,res) => {
     res.status(200).send({success: true})
 })
 
-app.listen(4000,() => {
-    console.log(`app is listening to port 4000`);
-})
+app.use('/requests', routes.requests);
+
+connectDb().then(async () => {
+    app.listen(process.env.PORT, () => {
+        console.log(`app is listening to port ${process.env.PORT}`);
+    });
+});
