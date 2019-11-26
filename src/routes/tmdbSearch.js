cab04ec00241629d63adae972ffe134652c7b0ae
Todd Jones <atoddj@gmail.com>
2019-11-25 19:32:27 -0500

    Added request status to tmdb search

--- src/routes/tmdbSearch.js
+++ src/routes/tmdbSearch.js
@@ -11,6 +11,14 @@ router.get('/', async (req, res) => {
                 query: req.query.q
             }
         });
+        let returnedResponse = await Promise.all(tmdbRes.data.results.map(async (tmdb) => {
+            let found = await req.context.models.Request.findOne({_id: tmdb.id});
+            if (found) {
+                return {...tmdb, status: found.status}
+            }
+            return tmdb;
+        }));
+        tmdbRes.data.results = returnedResponse;
         res.status(200).send(tmdbRes.data);
     } else {
         res.send({success: false, message: "Include search parameter 'q' in url with a search term."})
